58583f8ebf16a6619f70a5277e09a7b739252f75
adamrzky <adamrizky340@gmail.com>
2024-03-09 22:49:33 +0700

    logo footer

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -4,12 +4,31 @@
                             <div class="col-sm-6">
                                  SCM Group 2024
                             </div>
+                            
                             <div class="col-sm-6">
                                 <div class="text-sm-end d-none d-sm-block">
-                                    {{-- Crafted with <i class="mdi mdi-heart text-danger"></i> by PWA --}}
-                                    Pulcra Chemicals
+                                    <a href="index.html" class="logo logo-dark">
+                                        <span class="logo-sm">
+                                            <img src="{{ asset('backend/assets/images/logo-sm.png') }}" alt="logo-sm" height="10">
+                                        </span>
+                                        <span class="logo-lg">
+                                            <img src="{{ asset('backend/assets/images/logo-dark.png') }}" alt="logo-dark" height="10">
+                                        </span>
+                                    </a>
+                  
+                                    <a href="index.html" class="logoFooter logo-light">
+                                        {{-- <span class="logo-sm">
+                                            <img src="{{ asset('backend/assets/images/logo-sm.png') }}" alt="logo-sm-light"
+                                                height="40">
+                                        </span> --}}
+                                        <span class="logo-lg">
+                                            <img src="{{ asset('backend/assets/images/logo-light.png') }}" alt="logo-light"
+                                                height="40">
+                                        </span>
+                                    </a>
                                 </div>
                             </div>
+                          
                         </div>
                     </div>
                 </footer>
